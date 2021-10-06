613c46caa475002e1e0cd5dbcdd2f989c2d30176
tghill <tghill@uwaterloo.ca>
2021-10-06 16:43:02 -0400

    Update EBM results

--- Assignment01/compare_timeseries.py
+++ Assignment01/compare_timeseries.py
@@ -4,9 +4,13 @@ from matplotlib import pyplot as plt
 import matplotlib.dates as mdates
 import numpy as np
 
+import hourly_daily_averages
+
 time = np.load('data/daily/time.npy', allow_pickle=True)
 sr50 = np.load('data/daily/melt.npy')
-ebm = np.load('data/daily/ebm_melt.npy')
+hourly_ebm = np.load('data/hourly/ebm_melt.npy')
+ebm = hourly_daily_averages.average_hourly_to_daily(hourly_ebm)
+np.save('data/daily/ebm_melt.npy', ebm)
 ti = np.load('data/daily/ti_melt.npy')
 
 fig, ax = plt.subplots(figsize=(8, 4))
