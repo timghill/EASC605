5b63889f469289807d1e9a52886372af6e69914f
tghill <tghill@uwaterloo.ca>
2021-11-09 20:12:43 -0500

    Add sensitivity tests

--- Assignment02/case_valley_slope.py
+++ Assignment02/case_valley_slope.py
@@ -43,3 +43,39 @@ plt.tight_layout()
 fig2.savefig('valley_slope.png', dpi=600)
 
 print(np.max(H2[-1]))
+
+A_sens = 2.4e-24*np.array([0.5, 0.75, 1, 1.25, 1.5, 2])
+N_sens = len(A_sens)
+volumes = np.zeros(N_sens)
+for i,A in enumerate(A_sens):
+    print('Case A = ', A*1e24)
+    Hi = drive_ice_flow(tt, xc, h0, zb, zELA=1400, method='BE', A=A)
+    volumes[i] = np.sum(Hi[-1, :])
+
+fig, (ax, ax2) = plt.subplots(figsize=(8, 4), ncols=2, sharey=True)
+ax.scatter(A_sens/2.4e-24, volumes/volumes[2])
+ax.grid()
+ax.set_xlabel('A/A$_0$')
+ax.set_ylabel('Relative ice volume (-)')
+ax.text(-0.075, 1.05, 'a', transform=ax.transAxes, fontsize=14)
+print(volumes/volumes[2])
+
+Gamma_0 = 0.007/(365*86400)
+Gamma_sens = Gamma_0*np.array([0.5, 0.75, 1, 1.25, 1.5, 2])
+M_sens = len(Gamma_sens)
+volumes_gamma = np.zeros(M_sens)
+for i,G in enumerate(Gamma_sens):
+    print('case Gamma = ', G/Gamma_0)
+    Hi = drive_ice_flow(tt, xc, h0, zb, zELA=1400, method='BE', Gamma=G)
+    volumes_gamma[i] = np.sum(Hi[-1, :])
+
+ax2.scatter(Gamma_sens/Gamma_0, volumes_gamma/volumes_gamma[2])
+ax2.grid()
+ax2.set_xlabel('$\\Gamma/\\Gamma_0$')
+ax2.text(-0.075, 1.05, 'b', transform=ax2.transAxes, fontsize=14)
+print(volumes_gamma/volumes_gamma[2])
+
+
+fig.savefig('valley_sensitivity.png', dpi=600)
+
+plt.show()
